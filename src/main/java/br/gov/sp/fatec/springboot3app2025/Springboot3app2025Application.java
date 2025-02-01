3faa70005995b4312e1d38c9335f6dbb19a0e6c8
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-02-01 14:07:21 -0300

    adding src

---
+++ src/main/java/br/gov/sp/fatec/springboot3app2025/Springboot3app2025Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3app2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3app2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3app2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springboot3app2025/Springboot3app2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springboot3app2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3app2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
