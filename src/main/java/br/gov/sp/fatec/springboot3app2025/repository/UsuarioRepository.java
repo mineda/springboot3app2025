44f3341ca67b4c87c726076c8a3fb668ea933a4d
Emanuel Mineda Carneiro <mineda@gmail.com>
2025-04-22 13:35:29 +0000

    Início da configuração de segurança

--- src/main/java/br/gov/sp/fatec/springboot3app2025/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/springboot3app2025/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package br.gov.sp.fatec.springboot3app2025.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,20 +11,20 @@ import br.gov.sp.fatec.springboot3app2025.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
-    public Usuario findByNome(String nome);
+    public Optional<Usuario> findByNome(String nome);
 
     @Query("select u from Usuario u where u.nome = ?1")
-    public Usuario buscarPeloNome(String nome);
+    public Optional<Usuario> buscarPeloNome(String nome);
 
     public List<Usuario> findByNomeContains(String nome);
 
     @Query("select u from Usuario u where u.nome like %?1%")
     public List<Usuario> buscarPeloNomeContendo(String nome);
 
-    public Usuario findByNomeAndSenha(String nome, String senha);
+    public Optional<Usuario> findByNomeAndSenha(String nome, String senha);
 
     @Query("select u from Usuario u where u.nome = ?1 and u.senha = ?2")
-    public Usuario buscarPeloNomeESenha(String nome, String senha);
+    public Optional<Usuario> buscarPeloNomeESenha(String nome, String senha);
 
     public List<Usuario> findByAutorizacoesNome(String nomeAutorizacao);
     
